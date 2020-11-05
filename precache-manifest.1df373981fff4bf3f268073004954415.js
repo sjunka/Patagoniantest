2d66db4e76614cdc7427cc357452a1122978e736
Sergio Junca <sjunca@phase2technology.com>
2020-11-04 23:06:02 -0500

    Updates

---
+++ precache-manifest.1df373981fff4bf3f268073004954415.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1fdc5cb6197dac25ed608141f633437",
+    "url": "/Patagoniantest/index.html"
+  },
+  {
+    "revision": "f6cc31d188f443041078",
+    "url": "/Patagoniantest/static/css/2.c40d1409.chunk.css"
+  },
+  {
+    "revision": "154ddebb3f184d4ee003",
+    "url": "/Patagoniantest/static/css/main.d7d0582e.chunk.css"
+  },
+  {
+    "revision": "f6cc31d188f443041078",
+    "url": "/Patagoniantest/static/js/2.4f51c7d1.chunk.js"
+  },
+  {
+    "revision": "0fc61ce0ee2d491f41ee7634c63d9b90",
+    "url": "/Patagoniantest/static/js/2.4f51c7d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "154ddebb3f184d4ee003",
+    "url": "/Patagoniantest/static/js/main.fca29d71.chunk.js"
+  },
+  {
+    "revision": "8fb456ab8f94f5ac7316",
+    "url": "/Patagoniantest/static/js/runtime-main.dc3f1bb6.js"
+  }
+]);
